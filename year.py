b115a6a135fd362baddbc28224e901788a552448
Swapna-Karlawad <swapnakarlawad@gmail.com>
2026-01-16 09:14:00 +0530

    Update year.py

--- year.py
+++ year.py
@@ -4,7 +4,7 @@ if len(sys.argv) == 2:
     year = int(sys.argv[1])
 else:
     print("No input given - Using default value")
-    year = 2024
+    year = 2027
 is_leap = (year % 400 == 0) 
 
 print("The year", year, "is a leap year:", is_leap)
